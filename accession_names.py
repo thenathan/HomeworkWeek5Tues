51561aaf77cc1e133abe722d134acb556c74f077
Nathan Burroughs <thenathanburroughs@gmail.com>
2015-02-10 08:31:47 +0000

    Requirement two, search for words with d or e
    Prints out first all with a, then all with d or e

--- accession_names.py
+++ accession_names.py
@@ -5,3 +5,9 @@ accs = ["xkn59438", "yhdck2", "eihd39d9", "chdsye847", "hedle3455", "xjhd53e", "
 for acc in accs:
     if re.search(r"5", acc):
         print("\t" + acc)
+
+print ("\n")
+
+for acc in accs:
+    if re.search(r"(d|e)", acc):
+        print("\t" + acc)
