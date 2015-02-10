ffce8e91c1cc266b54e15a3e48712bf76cac83a8
Nathan Burroughs <thenathanburroughs@gmail.com>
2015-02-10 09:08:45 +0000

    The rest of the steps, the full code writes out:
    contains the number 5
    contain the letter d or e
    contain the ltters d and e in that order with a single letter between them
    contain the letters d and e in that order
    contain both the letters d and e in any order
    start with x or y
    start with x or y and end with e
    contain three or more numbers in a row
    end with d followed by either a, r, or p

--- accession_names.py
+++ accession_names.py
@@ -30,4 +30,32 @@ for acc in accs:
     if re.search(r"(d.e)", acc):
         print("\t" + acc)
 
+print ("\n")
 
+for acc in accs:
+    if re.search(r"^(x|y)", acc):
+        print("\t" + acc)
+
+print ("\n")
+
+for acc in accs:
+    if re.search(r"^(x|y).*e$", acc):
+        print("\t" + acc)
+
+print ("\n")
+
+for acc in accs:
+    if re.search(r"[0123456789]{3,100}", acc):
+        print("\t" + acc)
+
+print ("\n")
+
+for acc in accs:
+    if re.search(r"\d{3,}", acc):
+        print ("\t" + acc)
+
+print ("\n")
+
+for acc in accs:
+    if re.search(r"d[arp]$", acc):
+        print("\t" + acc)
